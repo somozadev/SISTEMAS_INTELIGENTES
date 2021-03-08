b727ca836358559f0e197e4cb3bd54701994e97b
somozadev <somozadev@gmail.com>
2021-03-08 12:19:34 +0100

    tokenizer & stemmer

--- corpus.py
+++ corpus.py
@@ -12,11 +12,20 @@ def LowerNTokenize(baseString):
         i = i+1
     return newBaseString
 
+def Stemmer(tokenized_text):
+    stemmer = SnowballStemmer('spanish')
+    stemmed_txt = [stemmer.stem(i) for i in tokenized_text]
+    print(stemmed_txt)
+    return stemmed_txt
 
 class main():
     
     testString = "Este es un texto de prueba para tokenizarlo"
     baseString = word_tokenize(testString,language="Spanish")
-
     baseString = LowerNTokenize(baseString)
     print(baseString)
+
+    
+    tokenizedString = Stemmer(baseString)
+
+
